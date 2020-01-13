b8db828cc2caeec60d1bdc3134a6351aab056834
Carleigh Crockett <capleugh@gmail.com>
2020-01-13 11:02:18 -0700

    Create uniqueness validation

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,6 @@
 class Flight <ApplicationRecord
-  validates_presence_of :number, :date, :time, :departure_city, :arrival_city
+  validates :number, uniqueness: true, presence: true
+  validates_presence_of :date, :time, :departure_city, :arrival_city
   belongs_to :airline
 
   has_many :flight_passengers
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :number }
+    it { should validate_uniqueness_of :number }
     it { should validate_presence_of :date }
     it { should validate_presence_of :time }
     it { should validate_presence_of :departure_city }
