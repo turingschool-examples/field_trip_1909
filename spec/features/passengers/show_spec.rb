ccd2b5d6e6fcbdedb362e387afef06a55598ddf4
Carleigh Crockett <capleugh@gmail.com>
2020-01-13 11:03:04 -0700

    Create test for making a form on passenger show page

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -25,5 +25,26 @@ RSpec.describe "As a visitor" do
 
       expect(current_path).to eq("/flights/#{southwest_2.id}")
     end
+
+    it "I see a form to add a flight and when I fill in said form with a flight number (which must be unique) and click submit, I am taken back to the passenger's show page and I can see the flight number I just added" do
+      southwest = Airline.create(name: "Southwest")
+      southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      southwest_2 = southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
+      southwest_3 = southwest.flights.create(number: "SW3", date: "12/08/20", time: "1000", departure_city: "Dallas", arrival_city: "Denver")
+      rando = Passenger.create(name: "Rando Randerson", age: 27)
+
+      rando.flights << [southwest_1, southwest_2]
+
+      visit "/passengers/#{rando.id}"
+
+      expect(page).to have_content("Add Another Flight:")
+
+      fill_in :number, with: "#{southwest_3.number}"
+
+      click_button 'Submit'
+
+      expect(current_path).to eq("/passengers/#{rando.id}")
+      expect(page).to have_content(southwest_3.number)
+    end
   end
 end
