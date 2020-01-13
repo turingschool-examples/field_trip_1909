610a2baf24186e4d6fd748082e1ed108606b3211
Santiago Caraveo <santiagocbrvg@hotmail.com>
2020-01-13 11:34:43 -0700

    addspart of 3

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/flights/:id', to: 'flights#show'
 
   get '/passengers/:id', to: 'passengers#show'
+  get 'passengers/:id/new/flight', to: 'passengers#new'
 end
