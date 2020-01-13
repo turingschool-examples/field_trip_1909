85052791307a7b15861f898d6deb3e762dc8c605
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-13 09:26:16 -0700

    Add relationship expectations to flights model test related to joins

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
   describe 'validations' do
-    it {should validate_presence_of :number}
-    it {should validate_presence_of :date}
-    it {should validate_presence_of :time}
-    it {should validate_presence_of :departure_city}
-    it {should validate_presence_of :arrival_city}
+    it { should validate_presence_of :number }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :time }
+    it { should validate_presence_of :departure_city }
+    it { should validate_presence_of :arrival_city }
   end
   describe 'relationships' do
-    it {should belong_to :airline}
+    it { should belong_to :airline}
+    it { should have_many :passenger_flights }
+    it { should have_many(:passengers).through(:passenger_flights) }
   end
 end
