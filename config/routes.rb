96ebf3008b626fc92576738c86434a039f271922
Carleigh Crockett <capleugh@gmail.com>
2020-01-13 09:36:48 -0700

    Create flight resources and controller and show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/airlines', to: 'airlines#index'
-  get '/airlines/:id', to: 'airlines#show'
+  # get '/airlines', to: 'airlines#index'
+  # get '/airlines/:id', to: 'airlines#show'
+
+  resources :airlines, only: [:index, :show]
+  resources :flights, only: [:show]
 end
